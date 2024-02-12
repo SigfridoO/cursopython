f3781e520fc402b79872e70c986979eb1230a39c
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-02-12 08:14:57 -0600

    mi primer commit

---
+++ Bases/Variables.py
@@ -0,0 +1,12 @@
+# Variables
+edad = 23
+print('edad:', edad)
+
+altura = 1.82
+print('altura: ', altura)
+
+fuma = False
+print ("fuma: ", fuma)
+
+nombre = 'Mi nombre es: "Isaac"'
+print ('nombre: ', nombre)
