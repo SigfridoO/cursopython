4209b886611d590de3925908a61c0c73716942e9
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-02-21 08:15:52 -0600

    creada la clase proyectil

--- POO/Proyectil.py
+++ POO/Proyectil.py
@@ -1,13 +1,35 @@
 
 
 class Proyectil:
+
+    # CONSTANTES
+    GRAVEDAD = 9.81
+
     # Constructor
-    def __init__(self):
-        print('Se ha creado el proyectil')
+    def __init__(self, nombre):
+        self.nombre = nombre
+        print(f'Se ha creado un objeto llamado {self.nombre}')
+
+    def establecer_parametros_iniciales(self, velocidadInicial, velocidadFinal, angulo):
+        # Propiedades
+        self.velocidadInicial = velocidadInicial
+        self.velocidadFinal = velocidadFinal
+        self.angulo = angulo
+
+    def calcular_altura(self):
+        y = (self.velocidadFinal * self.velocidadFinal - self.velocidadInicial * self.velocidadInicial) / (2 * self.GRAVEDAD)
+
+        print (f'y (altura de {self.nombre} )= ', y)
 
 def main():
     print('Dentro de main')
-    piedra = Proyectil()
+    piedra = Proyectil("piedra")
+    piedra.establecer_parametros_iniciales(0,50,0)
+    piedra.calcular_altura()
+
+    bala = Proyectil("bala")
+    bala.establecer_parametros_iniciales(0,100,0)
+    bala.calcular_altura()
 
 if __name__ == "__main__":
     main()
