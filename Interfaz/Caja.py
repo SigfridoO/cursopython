fe50d859a3235f845bf763fb061eea7a0951b439
sigfrido <sigfrido_o@ardilluda.com.mx>
2024-03-07 10:50:09 -0600

    agregado caja

---
+++ Interfaz/Caja.py
@@ -0,0 +1,26 @@
+
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel
+
+
+class Caja(QLabel):
+    def __init__(self, color):
+        super().__init__()
+        self.setStyleSheet(f'background-color: {color}')
+
+
+class Ventana(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        caja = Caja('green')
+        self.setCentralWidget(caja)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Ventana()
+    window.show();
+    sys.exit(app.exec())
+       
--- Interfaz/CampoNumerico.py
+++ Interfaz/CampoNumerico.py
@@ -1,12 +1,25 @@
 
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QDoubleSpinBox
 
 class Ventana(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
+        numero = QDoubleSpinBox()
+        #numero.setMaximum(8)
+        #numero.setMinimum(-3)
+        numero.setRange(-3, 8)
+        numero.setPrefix("Temp: ")
+        numero.setSuffix("º")
+        numero.setSingleStep(0.5)
+        numero.valueChanged.connect(self.valor_cambiado)
+
+        self.setCentralWidget(numero)
+
+    def valor_cambiado(self, valor):
+        print(valor)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -14,3 +27,4 @@ if __name__ == '__main__':
     window.show();
     sys.exit(app.exec())
        
+º
